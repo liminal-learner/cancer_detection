54cf0cc1709ed6d53be7accc5428adb839bf547e
Heather Victoria Champion <heather@helloaida.io>
2019-11-13 11:44:48 -0800

    Load pickle history for each loaded model.

--- src/model/ModelContainer.py
+++ src/model/ModelContainer.py
@@ -1,6 +1,7 @@
 from tensorflow import keras
 import os
 import matplotlib as plt
+import pickle
 
 class ModelContainer:
     """
@@ -26,21 +27,34 @@ class ModelContainer:
         # Adds a compiled model to the container
         self.models[model.name] = model
 
-    def get_auc(self, history, model_name):
+    def _get_auc(self, history, model_name):
         # For some reason the names of these keys change for multiple runs of the
         # training procedure:
         auc_label = list(history.history.keys())[1]
         val_auc_label = list(history.history.keys())[3]
-        self.roc_auc[model.name] = history.history[auc_label][-1] # 'auc'
-        self.val_roc_auc[model.name] = history.history[val_auc_label][-1] # 'val_auc'
+        self.roc_auc[model_name] = history.history[auc_label][-1] # 'auc'
+        self.val_roc_auc[model_name] = history.history[val_auc_label][-1] # 'val_auc'
 
     def load_model(self, model_path):
         # Loads a model from file and adds it to the container
         model = keras.models.load_model(model_path)
         self.add_model(model)
 
-        # This won't work - keras doesn't store the history. Will have to implement explicitly later.
-        # self.get_auc(model.history, model.name)
+        # Load the training history
+        with open(os.path.join(self.models_path, model.name, 'history'), "rb") as input_file:
+             self.history[model.name] = pickle.load(input_file)
+
+        # Load the metrics from the history
+        self._get_auc(self.history[model.name], model.name)
+
+    def save_model(self, model_name):
+        # Save to model to file:
+        path_to_model = os.path.join(self.models_path, model_name, model_name + ".h5")
+        self.models[model_name].save(path_to_model)
+
+        # Save the history as well as the model for plotting later if needed:
+        with open(os.path.join(self.models_path, model_name, 'history'), 'wb') as file_pi:
+            pickle.dump(history.history, file_pi)
 
     def train_model(self, data, model_name, num_epochs):
 
@@ -58,16 +72,7 @@ class ModelContainer:
                             #use_multiprocessing=True,
                             callbacks = [checkpoint_cb, reduce_LR_cb, early_stopping_cb])
 
-        self.get_auc(history, model.name)
-
-    def save_model(self, model_name):
-        # Save to model to file:
-        path_to_model = os.path.join(self.models_path, model_name + ".h5")
-        self.models[model_name].save(path_to_model)
-
-        # Save the history as well as the model for plotting later if needed:
-        with open('/trainHistoryDict', 'wb') as file_pi:
-            pickle.dump(history.history, file_pi)
+        self._get_auc(self.history[model_name], model_name)
 
     def select_best_model(self):
         # Selects and saves the best model based based on AUC on validation set:
